b2033ef5c4a06a9530ec4a5e4641455671bd5f1a
dungnguyenBKA <dungdasua365@gmail.com>
2021-11-15 23:08:49 +0700

    add base component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import AddPetScreen from './screen/add-pet/AddPetScreen'
 import DiscoveryScreen from './screen/community/DiscoveryScreen'
 import HomeScreen from './screen/home/HomeScreen'
 import LoginScreen from './screen/login/LoginScreen'
@@ -10,6 +11,7 @@ export default function App() {
                 <Route path="/" element={<LoginScreen/>} />
                 <Route path="/home" element={<HomeScreen/>} />
                 <Route path="/community" element={<DiscoveryScreen/>} />
+                <Route path="/add-pet" element={<AddPetScreen/>} />
             </Routes>
         </Router>
     )
