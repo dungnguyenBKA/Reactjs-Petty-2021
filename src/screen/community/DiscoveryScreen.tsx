5238455045806a26e9edff6dbbc5e72051f55c94
dat2403 <hoangtiendat.thidai@gmail.com>
2021-11-15 23:50:47 +0700

    add Like Share Cmt

--- src/screen/community/DiscoveryScreen.tsx
+++ src/screen/community/DiscoveryScreen.tsx
@@ -1,16 +1,26 @@
 import { Col, Container, Row } from "react-bootstrap"
+import Post from "./Post"
 
-interface DiscoveryScreenProp{
-    
+
+interface DiscoveryScreenProp {
+
 }
+
+let posts = [
+    { avatarUrl: "https://cuoifly.tuoitre.vn/820/0/ttc/r/2021/01/22/hai-tu-1611284734.jpg", petName: "Hai Tu", content: "Hải Tú và Thiều Bảo Trâm là hai mỹ nhân Việt được chú ý nhờ liên quan đến Sơn Tùng M-TP.", imgUrl: "https://photo-cms-kienthuc.zadn.vn/zoom/800/uploaded/dinhcuc/2020_12_21/tu/do-ve-goi-cam-hai-tu-va-ban-gai-tin-don-cua-son-tung.jpg" },
+    { avatarUrl: "https://cuoifly.tuoitre.vn/820/0/ttc/r/2021/01/22/hai-tu-1611284734.jpg", petName: "Hai Tu 1", content: "Hải Tú và Thiều Bảo Trâm là hai mỹ nhân Việt được chú ý nhờ liên quan đến Sơn Tùng M-TP.", imgUrl: "https://photo-cms-kienthuc.zadn.vn/zoom/800/uploaded/dinhcuc/2020_12_21/tu/do-ve-goi-cam-hai-tu-va-ban-gai-tin-don-cua-son-tung.jpg" },
+    { avatarUrl: "https://cuoifly.tuoitre.vn/820/0/ttc/r/2021/01/22/hai-tu-1611284734.jpg", petName: "Hai Tu 2", content: "Hải Tú và Thiều Bảo Trâm là hai mỹ nhân Việt được chú ý nhờ liên quan đến Sơn Tùng M-TP.", imgUrl: "https://photo-cms-kienthuc.zadn.vn/zoom/800/uploaded/dinhcuc/2020_12_21/tu/do-ve-goi-cam-hai-tu-va-ban-gai-tin-don-cua-son-tung.jpg" },
+]
 
 export default function DiscoveryScreen(prop: DiscoveryScreenProp) {
     return <Container fluid>
-        <Row>
-            <Col>1 of 1</Col>
-            <Col>2 of 1</Col>
-            <Col>3 of 1</Col>
-            <Col>4 of 1</Col>
-        </Row>
+        {
+            posts.map((postItem) => 
+            <Post 
+            petName={postItem.petName} avatarURL={postItem.avatarUrl} 
+            imgURL={postItem.imgUrl} content={postItem.content}
+            />
+            )
+        }
     </Container>
 }
