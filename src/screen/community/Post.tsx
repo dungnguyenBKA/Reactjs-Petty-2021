2c98eabded09baf7a64812af5eb46c7ded7ab08c
dat2403 <hoangtiendat.thidaihoc@gmail.com>
2021-12-09 09:46:09 +0700

    add Like (Liked) button in Post

--- src/screen/community/Post.tsx
+++ src/screen/community/Post.tsx
@@ -1,5 +1,24 @@
 import { Card, Image } from "react-bootstrap";
-import { AppStyle, background, borderWidth, flexCenter, flexCenterInParent, flexHori, height, marginEnd, marginTop, marginVertical, radius, semiBold, shadow, weightItem, width } from "../../AppStyle";
+import { FC, useState } from "react";
+
+import {
+  AppStyle,
+  background,
+  borderWidth,
+  flexCenter,
+  flexCenterInParent,
+  flexHori,
+  height,
+  marginEnd,
+  marginTop,
+  marginVertical,
+  radius,
+  semiBold,
+  shadow,
+  textColor,
+  weightItem,
+  width,
+} from "../../AppStyle";
 import { Colors } from "../../AppColor";
 import TextView from "../../components/Text";
 import Rows from "../../components/Row";
@@ -7,71 +26,116 @@ import Column from "../../components/Column";
 import MessengerScreen from "../messenger/MessengerScreen";
 import { useNavigate } from "react-router";
 
-
 interface PostProp {
-    petName: string,
-    content: string,
-    avatarURL: string,
-    imgURL: string
+  petName: string;
+  content: string;
+  avatarURL: string;
+  imgURL: string;
 }
 
 export default function Post(props: PostProp) {
-    let navigate = useNavigate()
-    
-    const messageHandler = () => {
-        navigate("../message")
-    }
-    
-    return <Card style={AppStyle(marginVertical(20), radius(8), shadow(2))}>
-        <Card.Body>
-            <Header avatarUrl={props.avatarURL} petName={props.petName} />
+  let navigate = useNavigate();
 
-            <Card.Text style={AppStyle(marginTop(12))}>
-                {props.content}
-            </Card.Text>
-        </Card.Body>
+  const messageHandler = () => {
+    navigate("../message");
+  };
 
-        <Card.Img variant="top" src={props.imgURL} />
+  return (
+    <Card style={AppStyle(marginVertical(20), radius(8), shadow(2))}>
+      <Card.Body>
+        <Header avatarUrl={props.avatarURL} petName={props.petName} />
 
-        <Card.Body>
+        <Card.Text style={AppStyle(marginTop(12))}>{props.content}</Card.Text>
+      </Card.Body>
 
-            <Column>
-                <hr style={marginVertical(12)} />
-                <Rows >
-                    <Reaction name={"Like"} />
-                    <Reaction name={"Comment"} />
-                    <button onClick = {messageHandler}>Message</button>
-                </Rows>
-                <hr style={marginVertical(12)} />
-            </Column>
+      <Card.Img variant="top" src={props.imgURL} />
 
-        </Card.Body>
+      <Card.Body>
+        <Column>
+          <hr style={marginVertical(12)} />
+          <Rows>
+            {/* <Reaction name={"Like"} /> */}
+            <Like />
+            <Reaction name={"Comment"} />
+            <button onClick={messageHandler}>Message</button>
+          </Rows>
+          <hr style={marginVertical(12)} />
+        </Column>
+      </Card.Body>
     </Card>
+  );
 }
 
 interface HeaderProp {
-    avatarUrl: string,
-    petName: string
+  avatarUrl: string;
+  petName: string;
 }
 
 function Header(props: HeaderProp) {
-    return (
-        <div style={AppStyle(flexHori(), flexCenter())}>
-            <Image src={props.avatarUrl} style={AppStyle(width(41), height(41), marginEnd(12))} roundedCircle />
-            <TextView style={AppStyle(semiBold(20))}>{props.petName}</TextView>
-        </div>
-    )
+  return (
+    <div style={AppStyle(flexHori(), flexCenter())}>
+      <Image
+        src={props.avatarUrl}
+        style={AppStyle(width(41), height(41), marginEnd(12))}
+        roundedCircle
+      />
+      <TextView style={AppStyle(semiBold(20))}>{props.petName}</TextView>
+    </div>
+  );
 }
 
 interface ReactionProp {
-    name: string
+  name: string;
 }
 
 function Reaction(props: ReactionProp) {
-    return (
-        <button style={AppStyle(weightItem(1), borderWidth(0), flexHori(), flexCenterInParent(),
-            background(Colors.color_white))}>
-            <TextView style={AppStyle(semiBold(15))}>{props.name}</TextView>
-        </button>
-    )
+  return (
+    <button
+      style={AppStyle(
+        weightItem(1),
+        borderWidth(0),
+        flexHori(),
+        flexCenterInParent(),
+        background(Colors.color_white)
+      )}
+    >
+      <TextView style={AppStyle(semiBold(15))}>{props.name}</TextView>
+    </button>
+  );
 }
+interface LikeProp {}
+
+const Like: FC<LikeProp> = () => {
+  const [isLike, setIsLike] = useState(false);
+
+  const clickHandler = () => {
+    if (isLike === true) {
+      setIsLike(false);
+    } else setIsLike(true);
+  };
+
+  return (
+    <div>
+      {!isLike && (
+        <button
+          style={AppStyle(background("#FFFFFF"), borderWidth(0))}
+          onClick={clickHandler}
+        >
+          Like
+        </button>
+      )}
+      {isLike && (
+        <button
+          style={AppStyle(
+            textColor("blue"),
+            background("#FFFFFF"),
+            borderWidth(0)
+          )}
+          onClick={clickHandler}
+        >
+          Liked
+        </button>
+      )}
+    </div>
+  );
+};
