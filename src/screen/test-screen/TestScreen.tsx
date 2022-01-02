dca9ab83ace6756d8131ccbdbed400409c811cca
dungnguyenBKA <dungdasua365@gmail.com>
2022-01-02 23:05:05 +0700

    update validate text

--- src/screen/test-screen/TestScreen.tsx
+++ src/screen/test-screen/TestScreen.tsx
@@ -1,6 +1,7 @@
-import {FC} from "react"
+import {FC, useState} from "react"
 import ValidateTextInput from "../../components/ValidatorInput";
 import Column from "../../components/Column";
+import {margin} from "../../AppStyle";
 
 
 interface TestScreenProp {
@@ -20,20 +21,34 @@ const TestScreen: FC<TestScreenProp> = (props) => {
 		return [text.length >= 8, 'password should min 8 chars']
 	}
 
+	let [email, setEmail] = useState('')
+	let [pwd, setPwd] = useState('')
+
 	return <Column>
 		<ValidateTextInput
+			style={
+				margin(16)
+			}
 			checkValidFunctions={[checkIsLenValid, checkIsContainValid]}
 			name={"Email"}
 			type="text"
 			placeholder={"Mail ne`"}
+			setValue={setEmail}
 		/>
 
 		<ValidateTextInput
+			style={
+				margin(16)
+			}
 			checkValidFunctions={[checkIsPwdValid]}
 			name={"Password"}
 			placeholder={"Password ne`"}
 			type="password"
+			setValue={setPwd}
 		/>
+
+		<h1>{email}</h1>
+		<h1>{pwd}</h1>
 	</Column>
 }
 
