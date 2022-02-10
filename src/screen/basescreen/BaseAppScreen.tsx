ab154593707e767ab7b4250004efc6fd10354b38
dungnguyenBKA <dungdasua365@gmail.com>
2022-02-10 17:36:43 +0700

    update last

--- src/screen/basescreen/BaseAppScreen.tsx
+++ src/screen/basescreen/BaseAppScreen.tsx
@@ -2,25 +2,46 @@ import {FC} from "react";
 import {Colors} from "../../AppColor";
 import {AppStyle, borderWidth} from "../../AppStyle";
 import {Props} from "../../components/Props";
+import Rows from "../../components/Row";
 import BaseScreen from "./BaseScreen";
+import {ImageView} from "../../components/ImageView";
 
 export const BaseMobileScreen: FC<Props<HTMLDivElement>> = (props) => {
-	return <BaseScreen
-		{...props}
+	return <Rows>
+		<ImageAds urlImage={'https://vcdn-vnexpress.vnecdn.net/2020/08/04/VinFast-President-1-9915-1596526753.jpg'}/>
 
-		style={
-			AppStyle(
-				{...props.style},
-				borderWidth(1),
-				{maxWidth: '768px', margin: 'auto', minHeight: "100vh", background: Colors.color_white}
-			)
-		}
+		<BaseScreen
+			{...props}
 
-	>
-		{
-			props.children
-		}
-	</BaseScreen>
+			style={
+				AppStyle(
+					{...props.style},
+					borderWidth(1),
+					{width: '50%', margin: 'auto', minHeight: "100vh", background: Colors.color_white}
+				)
+			}
+
+		>
+			{
+				props.children
+			}
+		</BaseScreen>
+
+		<ImageAds urlImage={'https://cellphones.com.vn/sforum/wp-content/uploads/2022/01/Samsung-Galaxy-S22-Ultra-S-Pen.jpg'}/>
+	</Rows>
+}
+
+interface ImageAdsProps {
+	urlImage: string
+}
+
+const ImageAds : FC<ImageAdsProps> = ({urlImage}) => {
+	return <ImageView
+		src={urlImage}
+		style={{
+		height: '100vh',
+			width: '25%'
+	}}/>
 }
 
 export const BaseFullScreen: FC<Props<HTMLDivElement>> = (props) => {
